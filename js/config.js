906de4c745fecb5728496c3277dc4e97c6a1b51d
Pablo Antonio Peña Mancia <3970131@gmail.com>
2026-05-29 04:36:53 -0600

    Update TASK service URL in config.js
    Signed-off-by: Pablo Antonio Peña Mancia <3970131@gmail.com>

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 const SERVICE_URLS = {
     // Microservicios de Google Apps Script (Producción Mayo 2026)
     USER: "https://script.google.com/macros/s/AKfycbzq4ApfdGiMktPy-Og4mikcA-GDQnlFXN31rOA8M-zrZfIzqZcZan7bWKUaPKlycDTi/exec",
-    TASK: "https://script.google.com/macros/s/AKfycbyEdU7HittAJy6Z6XeGITVUwBGBjBDWtXp3iAWl7vTgozXJKCDvB51HOxH36UW5OfEB/exec",
+    TASK: "https://script.google.com/macros/s/AKfycbw-laOw989DNU4Oe7c_uOl_2FmXAmE9ZFtomxz2-BY8laQFV6kHVB-GzdOWJe8u8cA1/exec",
     EXAM: "https://script.google.com/macros/s/AKfycbwKlOEB1WZAcgm9G7yD27YCEu5u7MjBbH9v839g7mpelaxmDEDouRgU1Ex1LAf-Bl4/exec"
 };
 
