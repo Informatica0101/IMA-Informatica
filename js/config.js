f3bed48fdecbc293510a1fb886ee8e78c2207fbc
Pablo Antonio Peña Mancia <3970131@gmail.com>
2026-05-29 04:43:32 -0600

    Update EXAM service URL in config.js
    Signed-off-by: Pablo Antonio Peña Mancia <3970131@gmail.com>

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ const SERVICE_URLS = {
     // Microservicios de Google Apps Script (Producción Mayo 2026)
     USER: "https://script.google.com/macros/s/AKfycbzq4ApfdGiMktPy-Og4mikcA-GDQnlFXN31rOA8M-zrZfIzqZcZan7bWKUaPKlycDTi/exec",
     TASK: "https://script.google.com/macros/s/AKfycbw-laOw989DNU4Oe7c_uOl_2FmXAmE9ZFtomxz2-BY8laQFV6kHVB-GzdOWJe8u8cA1/exec",
-    EXAM: "https://script.google.com/macros/s/AKfycbwKlOEB1WZAcgm9G7yD27YCEu5u7MjBbH9v839g7mpelaxmDEDouRgU1Ex1LAf-Bl4/exec"
+    EXAM: "https://script.google.com/macros/s/AKfycbzaF_8WHsoc-9Cki9qL4b8H66eKwucWSlKVDNy2gW-Z6EsRvgGRyQ7v7ngqEJzNKNVy/exec"
 };
 
 const CACHE_VERSION = 'v=21';
