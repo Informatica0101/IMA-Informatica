2ddfe9a1a350676dc7633e97f41e7ddbba076eaa
Pablo Antonio Peña Mancia <3970131@gmail.com>
2026-05-31 21:00:05 -0600

    Update TASK URL in config.js
    Signed-off-by: Pablo Antonio Peña Mancia <3970131@gmail.com>

--- js/config.js
+++ js/config.js
@@ -14,7 +14,7 @@ const SERVICE_URLS = {
   USER: 'https://script.google.com/macros/s/AKfycbyzY1FiMbhKapPxqQMOdi8i2tkNmlhTgbg24A-PJs8pSad5PiZ9DDYH9p7OUC_WvyQJ/exec',
 
   // Pega aquí la URL del despliegie del microservicio de tareas.
-  TASK: 'https://script.google.com/macros/s/AKfycbzaPvYowctE4CaUFiyFYwTUXx5W4thgzw5BM9Vj4hnAtpW0N3BIMYYlJ3S6WP19Kjlq/exec',
+  TASK: 'https://script.google.com/macros/s/AKfycbwbQIcblDcQgyWF1NFUNBpQfPOUzXcGeE-G4gT163WDwoUQBjlYHgO4Kz6E5j-BZFU/exec',
 
   // Pega aquí la URL del despliegue del microservicio de exámenes.
   EXAM: 'https://script.google.com/macros/s/AKfycbwyLYiXI3KHmBm8tr7-Gr8QXP-k5jPe8wlX622C8nvwRD2EV0Uu5ViwT6RVyLb4wz4/exec'
