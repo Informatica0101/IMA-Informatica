f3143762c37fce866fdb35ed95216d941dd2ccf7
Pablo Antonio Peña Mancia <3970131@gmail.com>
2026-06-02 05:23:11 -0600

    Update USER service URL in config.js
    Signed-off-by: Pablo Antonio Peña Mancia <3970131@gmail.com>

--- js/config.js
+++ js/config.js
@@ -11,7 +11,7 @@
 
 const SERVICE_URLS = {
   // Pega aquí la URL del despliegue del microservicio de usuarios.
-  USER: 'https://script.google.com/macros/s/AKfycbxUk5aPtkTl0vkqaPmtGtmCNC19pJ6g4nU7Pd73opRH0VBS4oWFVIAiegpXgTR0WoJ0/exec',
+  USER: 'https://script.google.com/macros/s/AKfycbz27i1GrswQvdt8EF9ssTdg9olLYb_jUCKsM6Ddw79FmnSiETKnIQLoDMO7TqSit0lD/exec',
 
   // Pega aquí la URL del despliegie del microservicio de tareas.
   TASK: 'https://script.google.com/macros/s/AKfycby2mX2UHrGe0KyhBMomwmgYNvHge2Yw0HON6_jm4-mCmYrkpiZHoerwxMUm0M4Nbtg/exec',
